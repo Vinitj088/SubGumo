b0344eaee4bef448193d031ad047d0d693d21ab7
Vinitj088 <vinitjain088@gmail.com>
2025-04-20 23:17:10 +0530

    added new hero destination card component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,12 +5,14 @@ import Destinations from "./Destinations";
 // Removed Footer import as it's likely in App.jsx or Layout
 import Testimonial from "./Testimonials";
 import TripCalendar from "./TripCalendar"; // Import the new calendar component
+import HeroDestinationCard from "./HeroDestinationCard"; // Import the new hero destination card component
 
 function Home() {
   return (
     <>
       <HeroSection />
       <Services />
+      <HeroDestinationCard />
       <Destinations />
       <TripCalendar /> {/* Add the calendar component here */} 
       <Testimonial />
